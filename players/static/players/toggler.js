4711f4faa7fff9238536b3a3992731552f69cdca
Flynn84 <arkchib@gmail.com>
2020-05-19 12:41:10 +0300

    Implement and use function for aligning blocks
    Set all blocks width equal the widthh of the widest

--- players/static/players/toggler.js
+++ players/static/players/toggler.js
@@ -1,8 +1,24 @@
-$('div.expander').click(function(){
-    $(this).next('.content').toggle();
+$('div.expander').click(function() {
+	content_div = $(this).next('.content');
+    content_div.toggle();
+    div_id = content_div.attr('id');
+
+    if (div_id) {
+    	div = $(`#${div_id}`);
+		set_to_widest(div);
+    };
 });
+
+function set_to_widest(div) {
+	labels = div.find('label');
+	if (labels.is(':visible')) {
+	  var widest_label = Math.max.apply(Math, labels.map(function() { 
+	  return $(this).width(); 
+    }));
+	  labels.width(widest_label);
+};
+};
 
 $('p.expander').click(function(){
 	$('.content').toggle();
 });
-
