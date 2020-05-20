bd4d71e79ee6a413a217b4a0440a66b9110cff5d
Flynn84 <arkchib@gmail.com>
2020-05-20 18:26:42 +0300

    Rewrite script to make it work with the new api endpoints
    Also implemented loading 100 rows per request, because now it is not available as all rows at a time

--- players/management/commands/upd_pls.py
+++ players/management/commands/upd_pls.py
@@ -14,19 +14,21 @@ from datetime import date
 import players.utils as utils
 from players.models import Goalie, Skater
 
-URL_PLAYERS = 'http://www.nhl.com/stats/rest/{}'
+URL_PLAYERS = 'https://api.nhle.com/stats/rest/en/{}/{}'
 URL_PLAYERS_PICS = 'https://nhl.bamcontent.com/images/headshots/current/168x168/{}.jpg'
-PL_TYPE1 = "goalies"
-PL_TYPE2 = "skaters"
-REP_TYPE1 = 'goaliesummary'
-REP_TYPE2 = 'skatersummary'
+PL_TYPE1 = "goalie"
+PL_TYPE2 = "skater"
+REP_TYPE1 = 'bios'
+REP_TYPE2 = 'summary'
 REP_TYPE3 = 'realtime'
 REP_TYPE4 = 'timeonice'
+REP_TYPE5 = 'faceoffwins'
 PLAYERS_PICS_DIR = 'players_pics'
 FLAGS_DIR = 'flags'
 INCH_TO_CM_COEFF = 2.54
 POUND_TO_KG_COEFF = 2.205
 SEASON = '20192020'
+PLAYERS_PER_PAGE = 100
 
 # Find a table with this, or transfer the dict to the external file
 COUNTRIES = {
@@ -71,114 +73,150 @@ class Command(BaseCommand):
         Returns:
 
         """
+        # print(player)
+
+        # bios report
+        # index = 0, index = 2
         id_ = player["playerId"]
+        if index == 0 or index == 2:
 
-        if player['playerBirthStateProvince'] is None:
-            player['playerBirthStateProvince'] = ''
+            if player['birthStateProvinceCode'] is None:
+                player['birthStateProvinceCode'] = ''
 
-        birth_date = parse_date(player["playerBirthDate"])
+            if player["nationalityCode"] is None:
+                player["nationalityCode"] = player["birthCountryCode"]
 
-        defaults = {
-            'name': get_char_field_value(player, "playerName"),
-            'first_name': get_char_field_value(player, "playerFirstName"),
-            'last_name': get_char_field_value(player, "playerLastName"),
-            'weight': player["playerWeight"],
-            'weight_kg': round(player["playerWeight"] / POUND_TO_KG_COEFF),
-            'birth_date': birth_date,
-            'birth_city': get_char_field_value(player, "playerBirthCity"),
-            'birth_state': get_char_field_value(player, "playerBirthStateProvince"),
-            'birth_country': COUNTRIES[player["playerBirthCountry"]],
-            'birth_country_abbr': get_char_field_value(player, "playerBirthCountry"),
-            'nation': COUNTRIES[player["playerNationality"]],
-            'nation_abbr': get_char_field_value(player, "playerNationality"),
-            'games': player["gamesPlayed"],
-            'height_cm': round(player['playerHeight'] * INCH_TO_CM_COEFF),
-            'age': calculate_age(birth_date),
-        }
+           # REMOVE
+           if player["lastName"] == 'Quinney':
+                print(player["birthDate"])
 
-        defaults_dr = {}
-        if player['playerDraftOverallPickNo']:
-            defaults_dr = {
-                'draft_year': player["playerDraftYear"],
-                'draft_round': player['playerDraftRoundNo'],
-                'draft_number': player['playerDraftOverallPickNo'],
+            birth_date = parse_date(player["birthDate"])
+
+            # 1 diff fields for skater and goalie:
+            # skaterFullName
+            # goalieFullName
+
+            # 2 no firstName field, parse from fullNAME
+
+            defaults = {
+                'last_name': get_char_field_value(player, "lastName"),
+                'weight': player["weight"],
+                'weight_kg': round(player["weight"] / POUND_TO_KG_COEFF),
+                'birth_date': birth_date,
+                'birth_city': get_char_field_value(player, "birthCity"),
+                'birth_state': get_char_field_value(player, "birthStateProvinceCode"),
+                'birth_country': COUNTRIES[player["birthCountryCode"]],
+                'birth_country_abbr': get_char_field_value(player, "birthCountryCode"),
+                'nation': COUNTRIES[player["nationalityCode"]],
+                'nation_abbr': get_char_field_value(player, "nationalityCode"),
+                'games': player["gamesPlayed"],
+                'height_cm': round(player['height'] * INCH_TO_CM_COEFF),
+                'age': calculate_age(birth_date),
             }
 
-        defaults = {**defaults, **defaults_dr}
+            defaults_dr = {}
+            if player['draftOverall']:
+                defaults_dr = {
+                    'draft_year': player["draftYear"],
+                    'draft_round': player['draftRound'],
+                    'draft_number': player['draftOverall'],
+                }
 
-        if player["playerPositionCode"] == utils.POSITIONS[0]:
-            defaults_g = {
-                'wins': player["wins"],
-                'losses': player["losses"],
-                'ot_losses': player["otLosses"],
-                'goals_against_av': player["goalsAgainstAverage"],
-                'saves_perc': player["savePctg"],
-                'saves': player["saves"],
-                'shotouts': player["shutouts"],
-            }
-            defaults = {**defaults, **defaults_g}
-            player_obj = Goalie.objects.update_or_create(
-                nhl_id=id_, defaults=defaults)[0]
+            defaults = {**defaults, **defaults_dr}
+
+            # TO CONSTANT
+            if 'positionCode' in player:
+                player_obj = Skater.objects.update_or_create(nhl_id=id_, defaults=defaults)[0]
+            else:
+                player_obj = Goalie.objects.update_or_create(nhl_id=id_, defaults=defaults)[0]
+
             img_name = f'{player_obj.slug}.jpg'
             flag_name = f'{player_obj.nation_abbr}.jpg'
 
             if pic_missing(img_name, player_obj.image, PLAYERS_PICS_DIR):
                 upload_pic(player_obj, img_name, URL_PLAYERS_PICS)
             if pic_missing(flag_name, player_obj.nation_flag, FLAGS_DIR):
+                print(flag_name)
                 upload_flag(player_obj, flag_name)
 
-        else:
-            if index == 1:
-                defaults_s = {
-                    'goals': player["goals"],
-                    'goals_avg': round(player["goals"] / player["gamesPlayed"], 2),
-                    'assists': player["assists"],
-                    'assists_avg': round(player["assists"] / player["gamesPlayed"], 2),
-                    'points': player["points"],
-                    'points_avg': round(player["points"] / player["gamesPlayed"], 2),
-                    'plus_minus': player["plusMinus"],
-                    'plus_minus_avg': round(player["plusMinus"] / player["gamesPlayed"], 2),
-                    'penalty_min': player["penaltyMinutes"],
-                    'penalty_min_avg': round(player["penaltyMinutes"] / player["gamesPlayed"], 2),
-                    'shots': player["shots"],
-                    'shots_avg': round(player["shots"] / player["gamesPlayed"], 2),
-                    'pp_points': player["ppPoints"],
-                    'pp_points_avg': round(player["ppPoints"] / player["gamesPlayed"], 2),
-                    'sh_points': player["shPoints"],
-                    'sh_points_avg': round(player["shPoints"] / player["gamesPlayed"], 2),
-                }
-                defaults = {**defaults, **defaults_s}
-                player_obj = Skater.objects.update_or_create(
-                    nhl_id=id_, defaults=defaults)[0]
-                img_name = f'{player_obj.slug}.jpg'
-                flag_name = f'{player_obj.nation_abbr}.jpg'
+        # goalie bios has no sv, gaa, sv%
 
-                if pic_missing(img_name, player_obj.image, PLAYERS_PICS_DIR):
-                    upload_pic(player_obj, img_name, URL_PLAYERS_PICS)
-                if pic_missing(flag_name, player_obj.nation_flag, FLAGS_DIR):
-                    upload_flag(player_obj, flag_name)
+        elif index == 1:  # goalie summary report
+        # if player["playerPositionCode"] == utils.POSITIONS[0]:
+            defaults = {
+                'name': get_char_field_value(player, "goalieFullName"),
+                # 'first_name': get_char_field_value(player, "playerFirstName"),
+                'wins': player["wins"],
+                'losses': player["losses"],
+                'ot_losses': player["otLosses"],
+                'shotouts': player["shutouts"],
+                'goals_against_av': player["goalsAgainstAverage"],
+                'saves_perc': player["savePct"],
+                'saves': player["saves"],
+            }
 
-            elif index == 2:
-                defaults = {
-                    'hits': player["hits"],
-                    'hits_avg': round(player["hits"] / player["gamesPlayed"], 2),
-                    'blocks': player["blockedShots"],
-                    'blocks_avg': round(player["blockedShots"] / player["gamesPlayed"], 2),
-                    'faceoff_wins': player["faceoffsWon"],
-                    'faceoff_wins_avg': round(player["faceoffsWon"] / player["gamesPlayed"], 2),
-                }
-                player_obj = Skater.objects.update_or_create(
-                    nhl_id=id_, defaults=defaults)[0]
+            Goalie.objects.update_or_create(nhl_id=id_, defaults=defaults)
 
-            elif index == 3:
-                defaults = {
-                    'time_on_ice': time_from_sec(player["timeOnIcePerGame"]),
-                    'time_on_ice_pp': time_from_sec(player["ppTimeOnIcePerGame"]),
-                    'time_on_ice_sh': time_from_sec(player["shTimeOnIcePerGame"]),
-                }
-                player_obj = Skater.objects.update_or_create(
-                    nhl_id=id_, defaults=defaults)[0]
+        elif index == 3:  # skater summary
+            defaults = {
+                'name': get_char_field_value(player, "skaterFullName"),
+                'goals': player["goals"],
+                'goals_avg': round(player["goals"] / player["gamesPlayed"], 2),
+                'assists': player["assists"],
+                'assists_avg': round(player["assists"] / player["gamesPlayed"], 2),
+                'points': player["points"],
+                'points_avg': round(player["points"] / player["gamesPlayed"], 2),
+                'plus_minus': player["plusMinus"],
+                'plus_minus_avg': round(player["plusMinus"] / player["gamesPlayed"], 2),
+                'penalty_min': player["penaltyMinutes"],
+                'penalty_min_avg': round(player["penaltyMinutes"] / player["gamesPlayed"], 2),
+                'shots': player["shots"],
+                'shots_avg': round(player["shots"] / player["gamesPlayed"], 2),
+                'pp_points': player["ppPoints"],
+                'pp_points_avg': round(player["ppPoints"] / player["gamesPlayed"], 2),
+                'sh_points': player["shPoints"],
+                'sh_points_avg': round(player["shPoints"] / player["gamesPlayed"], 2),
+            }
 
+            Skater.objects.update_or_create(nhl_id=id_, defaults=defaults)
+
+            # img_name = f'{player_obj.slug}.jpg'
+            # flag_name = f'{player_obj.nation_abbr}.jpg'
+
+            # if pic_missing(img_name, player_obj.image, PLAYERS_PICS_DIR):
+            #     upload_pic(player_obj, img_name, URL_PLAYERS_PICS)
+            # if pic_missing(flag_name, player_obj.nation_flag, FLAGS_DIR):
+            #     upload_flag(player_obj, flag_name)
+
+        elif index == 4:
+            defaults = {
+                'hits': player["hits"],
+                'hits_avg': round(player["hits"] / player["gamesPlayed"], 2),
+                'blocks': player["blockedShots"],
+                'blocks_avg': round(player["blockedShots"] / player["gamesPlayed"], 2),
+            }
+
+            Skater.objects.update_or_create(nhl_id=id_, defaults=defaults)
+
+        elif index == 5:
+            defaults = {
+                'time_on_ice': time_from_sec(player["timeOnIcePerGame"]),
+                'time_on_ice_pp': time_from_sec(player["ppTimeOnIcePerGame"]),
+                'time_on_ice_sh': time_from_sec(player["shTimeOnIcePerGame"]),
+            }
+
+            Skater.objects.update_or_create(nhl_id=id_, defaults=defaults)
+
+        elif index == 6:
+            defaults = {
+                'faceoff_wins': player["totalFaceoffWins"],
+                'faceoff_wins_avg': round(player["totalFaceoffWins"] / player["gamesPlayed"], 2),
+            }
+
+            Skater.objects.update_or_create(nhl_id=id_, defaults=defaults)    
+
+
+        # FACEOFFS and TOI HAVE A DIFFERENT ORDER
     def handle(self, *args, **options):
         """
 
@@ -190,18 +228,29 @@ class Command(BaseCommand):
 
         """
         reports_list = [
-            [REP_TYPE1, PL_TYPE1],
-            [REP_TYPE2, PL_TYPE2],
-            [REP_TYPE3, PL_TYPE2],
-            [REP_TYPE4, PL_TYPE2],
+            [REP_TYPE1, PL_TYPE1],  # 0 bios goalies
+            [REP_TYPE2, PL_TYPE1],  # 1 summary goalies
+            [REP_TYPE1, PL_TYPE2],  # 2 bios skaters
+            [REP_TYPE2, PL_TYPE2],  # 3 summary skaters
+            [REP_TYPE3, PL_TYPE2],  # 4 realtime skaters
+            [REP_TYPE4, PL_TYPE2],  # 5 timeonice skaters
+            [REP_TYPE5, PL_TYPE2],  # 6 faceoffs skaters
         ]
 
         for index, item in enumerate(reports_list):
-            print(f'\n Uploading from {item[0]} report')
-            data = players_resp(item[0], item[1]).json()["data"]
-            # print(players_resp(item[0], item[1]).json())
+            print(f'\n Uploading from {item[1]} {item[0]} report')
+            start = 0   # TO CONSTANT
+            data = players_resp(item[0], item[1], start).json()
+            total_players = data['total']
+            players_list = data['data']
+            if total_players > PLAYERS_PER_PAGE:
+                start += PLAYERS_PER_PAGE
+                while start < total_players:
+                    resp = players_resp(item[0], item[1], start).json()['data']
+                    start += PLAYERS_PER_PAGE
+                    players_list += resp
 
-            for player in tqdm(data):
+            for player in tqdm(players_list):
                 self.import_player(player, index)
 
 
@@ -291,9 +340,8 @@ def time_from_sec(time):
     return f'{min_}:{sec}'.rjust(5, '0')
 
 
-def players_resp(rep_type, pl_type):
+def players_resp(rep_type, pl_type, start_from_index):
     """
-
     Args:
       rep_type:
       pl_type:
@@ -303,10 +351,15 @@ def players_resp(rep_type, pl_type):
     """
     params = {
         'isAggregate': 'false',
-        'reportType': 'season',
+        # 'reportType': 'season',
         'isGame': 'false',
-        'reportName': rep_type,
+        'limit': PLAYERS_PER_PAGE,
+        'start': start_from_index,
+        # 'reportName': rep_type,
         'cayenneExp': f'gameTypeId=2 and seasonId={SEASON}',
     }
 
-    return requests.get(URL_PLAYERS.format(pl_type), params=params)
+    return requests.get(URL_PLAYERS.format(pl_type, rep_type), params=params)
+
+
+# https://api.nhle.com/stats/rest/en/skater/realtime?isAggregate=false&isGame=false&sort=%5B%7B%22property%22:%22hits%22,%22direction%22:%22DESC%22%7D%5D&start=0&limit=100&factCayenneExp=gamesPlayed%3E=1&cayenneExp=gameTypeId=2%20and%20seasonId%3C=20192020%20and%20seasonId%3E=20192020
