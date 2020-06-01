e15495527c281aa4691818185d48be6a80ab3422
--global <arkchib@gmail.com>
2020-06-01 11:32:13 +0300

    Add an example of a email confirmation celery task

--- players/tasks.py
+++ players/tasks.py
@@ -20,3 +20,14 @@ def upd_pls():
 @shared_task
 def upd_tms():
     management.call_command('upd_tms')
+
+
+# @celery.task
+# def send_confirmation_mail(user_id):
+#     user = User.get(User.pk == user_id)
+#     send_mail(
+#         to=user.email,
+#         subject='[Selfmailbot] Confirm your email',
+#         user_id=user.id,
+#         text=get_template('email/confirmation.txt').render(user=user),
+#     )
