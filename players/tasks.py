52f5901b6212fc2658c5fbe9ea35b4c695345a6f
--global <arkchib@gmail.com>
2023-11-17 22:45:42 +0300

    Make one big task for all database upload commands

--- players/tasks.py
+++ players/tasks.py
@@ -4,22 +4,10 @@ from django.core import management
 
 
 @shared_task
-def upd_pls():
+def upd_database():
     management.call_command('upd_tms')
     management.call_command('upd_pls')
-    management.call_command('upd_pls_tot')
     management.call_command('upd_gms')
-    management.call_command('upd_pls_sbs')
-    management.call_command('upd_pls_proj')
-    # management.call_command('upd_pls_fw')
-    # management.call_command('upd_pls_multi_g')
-    # management.call_command('upd_pls_logs_fw')
-    # management.call_command('check_sbs')
-
-
-@shared_task
-def upd_tms():
-    management.call_command('upd_tms')
 
 
 # @celery.task
