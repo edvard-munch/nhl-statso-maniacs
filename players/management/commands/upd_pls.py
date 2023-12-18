04eccacbee8825e1eca0ace1350cbcd132148258
--global <arkchib@gmail.com>
2023-12-18 23:43:30 +0300

    Exception attributes not used, so `as e` not neeed

--- players/management/commands/upd_pls.py
+++ players/management/commands/upd_pls.py
@@ -275,7 +275,7 @@ def get_response(rep_type, pl_type, start_from_index):
         print(e.code)
         print('Wrong endpoint or a problem with the connection!')
         return None
-    except requests.exceptions.ReadTimeout as e:
+    except requests.exceptions.ReadTimeout:
         print('Timeout!')
         return None
 
